d60d4fb7eb3a602bd98955a05993f3635378d469
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-30 11:06:51 +0530

    Much optimized the code. Now everything is faster

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,35 +1,49 @@
-import React, { useContext, useState } from "react";
+/* eslint-disable */
+
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import faker from "faker";
 import firebase from "firebase";
 import "firebase/firestore";
 import { Button } from "react-bootstrap";
+import { v4 as uuid } from "uuid";
+import { toast } from "react-toastify";
 
 const UserData = () => {
 	const [authState] = useContext(AuthContext);
 
 	const [dbItems, setDbItems] = useState();
 
-	const db = firebase.firestore().collection("things");
+	const db = firebase.firestore();
+
+	const thingsDb = db.collection("things");
 
 	const addItemToDb = () => {
-		db.add({
-			uid: authState.user.uid,
-			name: faker.commerce.productName(),
-		});
+		const newItem = faker.commerce.productName();
+		thingsDb
+			.add({
+				itemid: uuid(),
+				uid: authState.user.uid,
+				name: newItem,
+			})
+			.then(() => {
+				toast.success(`✅ "${newItem}" added`);
+			});
 	};
 
-	let unsubscribe;
-	if (authState.user) {
-		unsubscribe = db.where("uid", "==", authState.user.uid).onSnapshot((querySnapshot) => {
-			const items = querySnapshot.docs.map((doc) => {
-				return doc.data().name;
+	useEffect(async () => {
+		let unsubscribe;
+		if (authState.user) {
+			unsubscribe = thingsDb.where("uid", "==", authState.user.uid).onSnapshot((querySnapshot) => {
+				const items = querySnapshot.docs.map((doc) => {
+					return doc.data();
+				});
+				setDbItems(items);
 			});
-			setDbItems(items);
-		});
-	} else {
-		unsubscribe && unsubscribe();
-	}
+		} else {
+			unsubscribe && unsubscribe();
+		}
+	}, [authState.user]);
 
 	const render = () => {
 		if (authState.user) {
@@ -37,10 +51,12 @@ const UserData = () => {
 				<div>
 					<p>Logged in as {authState.user.displayName}</p>
 					<Button onClick={addItemToDb}>Add a Random Item</Button>
-					{dbItems &&
-						dbItems.map((item) => {
-							return <li key={item.id}>{item}</li>;
-						})}
+					<ul>
+						{dbItems &&
+							dbItems.map((item) => {
+								return <li key={item.itemid}>{item.name}</li>;
+							})}
+					</ul>
 				</div>
 			);
 		}
