15f3b6ff335af82d36897364208c4791fe04d2f6
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-30 10:22:32 +0530

    Items display on screen. But they hella slow

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,11 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import faker from "faker";
+import firebase from "firebase";
+import "firebase/firestore";
+import { Button } from "react-bootstrap";
 
 const UserData = () => {
 	const [authState] = useContext(AuthContext);
 
+	const [dbItems, setDbItems] = useState();
+
+	const db = firebase.firestore().collection("things");
+
+	const addItemToDb = () => {
+		db.add({
+			uid: authState.user.uid,
+			name: faker.commerce.productName(),
+		});
+	};
+
+	if (authState.user) {
+		db.where("uid", "==", authState.user.uid).onSnapshot((querySnapshot) => {
+			const items = querySnapshot.docs.map((doc) => {
+				return doc.data().name;
+			});
+			setDbItems(items);
+		});
+	}
+
 	const render = () => {
-		return authState.user && `Logged in as ${authState.user.displayName}`;
+		if (authState.user) {
+			return (
+				<div>
+					<p>Logged in as {authState.user.displayName}</p>
+					<Button onClick={addItemToDb}>Add a Random Item</Button>
+					{dbItems &&
+						dbItems.map((item) => {
+							return <li key={item.id}>{item}</li>;
+						})}
+				</div>
+			);
+		}
 	};
 
 	return <React.Fragment>{render()}</React.Fragment>;
